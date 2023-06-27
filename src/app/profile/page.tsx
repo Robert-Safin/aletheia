02fa215ed41dc6ed7f2e730d7a0706920d7ec91b
Robert Safin <robertsafin98@gmail.com>
2023-06-27 23:05:36 +0800

    links

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,17 @@
+import useCustomServerSession from "@/lib/useCustomServerSession";
+import { FC } from "react";
+
+
+const ProfilePage:FC = async (props) => {
+
+
+  return (
+    <>
+      <div>
+        <h1>Profile Page</h1>
+      </div>
+    </>
+  );
+}
+
+export default ProfilePage;
