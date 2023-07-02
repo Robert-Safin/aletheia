063aaf4199c0663f254f4cb9898b015ec34ef10d
Robert Safin <robertsafin98@gmail.com>
2023-07-02 17:37:18 +0800

    pop up deisgn

--- src/components/popups/ErrorPopup.tsx
+++ src/components/popups/ErrorPopup.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import styles from './ErrorPopup.module.css'
+import { PiWarningCircleBold } from "react-icons/pi";
 
 interface Props {
   message : string
@@ -9,6 +10,7 @@ interface Props {
 const ErrorPopup:FC<Props> = (props) => {
   return (
     <div className={styles.popup}>
+      <PiWarningCircleBold className={styles.icon}/>
       <p className={styles.message}>{props.message}</p>
     </div>
   )
